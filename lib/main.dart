4e7a9b9afd557ad71db10bc98e49e9105d66af40
aadil <aadilhasan9697@gmail.com>
2022-11-20 09:20:19 +0530

    Updated_Quiz_App

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+import './quiz.dart';
+import 'result.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1}
+      ]
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 10},
+        {'text': 'Lion', 'score': 1},
+        {'text': 'Snake', 'score': 3},
+        {'text': 'Elephant', 'score': 4}
+      ]
+    },
+    {
+      'questionText': 'What\'s your favorite Country?',
+      'answers': [
+        {'text': 'India', 'score': 10},
+        {'text': 'America', 'score': 4},
+        {'text': 'Australia', 'score': 3},
+        {'text': 'Turkey', 'score': 1}
+      ]
+    }
+  ];
+  var _questionIndex = 0;
+  var _totalscore = 0;
+
+  void _answerQuestion(int score) {
+    // var aBool = true;
+    // aBool = false;
+    _totalscore += score;
+
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have more questions!');
+    } else {
+      print('No more questions!');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(),
+      ),
+    );
+  }
+}
